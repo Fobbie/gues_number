8efc5e68a15f39c0ccf545eb9de32012057c7877
Fobb <72540951+Fobbie@users.noreply.github.com>
2023-07-29 22:21:50 +0300

    Add files via upload

---
+++ traning.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <random>
+using namespace std;
+int number_gues(){
+	random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dist(1, 100);
+
+  int randomNumber = dist(gen);
+  int number{};
+  int counter{};
+  bool cond = true;
+
+  cout << "Try to guess number from 1 to 100--> ";
+  while (cond) {
+    ++counter;
+    cin >> number;
+    if (number == randomNumber) {
+      cout << "U make " << counter << " tries!!";
+      cond = false;
+    } else if (number < randomNumber && (randomNumber - number) <= 10) {
+      cout << "U r so close little bit higher) " << endl;
+    } else if (number < randomNumber && (randomNumber - number) <= 25) {
+      cout << "U r close  bit higher) " << endl;
+    } else if (number < randomNumber && (randomNumber - number) < 35) {
+      cout << "Nah man higher) " << endl;
+    } else if (number > randomNumber && (number - randomNumber) <= 10) {
+      cout << "U r so close little bit lower) " << endl;
+    } else if (number > randomNumber && (number - randomNumber) <= 25) {
+      cout << "U r close bit lower) " << endl;
+    } else if (number > randomNumber && (number - randomNumber) < 35) {
+      cout << "Nah man lower) " << endl;
+    }
+  }
+  return counter;
+}
+int main() {
+  
+	number_gues();
+  return 0;
+}
